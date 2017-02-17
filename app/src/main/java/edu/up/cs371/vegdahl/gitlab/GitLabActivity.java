202ccff1153e74d1363bedd423b7ddd8dccb0c56
sterba19 <sterba19@up.edu>
2017-02-16 21:34:21 -0800

    Random Button merged

--- app/src/main/java/edu/up/cs371/vegdahl/gitlab/GitLabActivity.java
+++ app/src/main/java/edu/up/cs371/vegdahl/gitlab/GitLabActivity.java
@@ -24,7 +24,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.Random;
 
